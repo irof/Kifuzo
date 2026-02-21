725cbce544c7848ad3a1d65063b8970d7b3acce4
irof <irof.ocean@gmail.com>
2026-02-22 01:23:53 +0900

    ktlintの関数命名規則チェックを有効化し、Composable関数のPascalCaseを許可する設定を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ spotless {
         ktlint().editorConfigOverride(
             mapOf(
                 "ktlint_standard_no-wildcard-imports" to "disabled",
-                "ktlint_standard_function-naming" to "disabled",
-                "ktlint_standard_blank-line-between-when-conditions" to "disabled"
+                "ktlint_standard_blank-line-between-when-conditions" to "disabled",
+                "ktlint_function_naming_ignore_when_annotated_with" to "Composable"
             )
         )
         trimTrailingWhitespace()
